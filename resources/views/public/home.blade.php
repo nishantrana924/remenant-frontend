023a939ece03599b1881039b319f335813aa8c9d
Prashant Tharu <prashanttharu@Prashants-MacBook-Air.local>
2026-04-14 22:45:21 +0530

    main file

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Open+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    <!-- Tailwind CSS -->
+    <!-- Tailwind CSS heloo -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <style>
         body { overflow-x: hidden; }
